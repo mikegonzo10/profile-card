a621371717d92fc349962f6d2ae8aa20cfe200b6
mikegonzo10 <95885681+mikegonzo10@users.noreply.github.com>
2022-02-23 14:07:13 -0800

    Add files via upload

---
+++ profile-card copy/src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    
+<div className = 'footer'>  
+           
+    <div className = 'stats'> 
+     <p> 389 Mil </p>
+     <p> Followers </p>
+   </div>
+   
+   <div className = 'stats'> 
+     <p> 102 Mil </p>
+     <p> Likes </p>
+   </div>
+
+   <div className = 'stats'> 
+     <p> 983 </p>
+     <p> Photos </p>
+   </div>
+   
+</div>
+  )
+}
---
+++ profile-card copy/src/components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Main() {
+  return (
+    <div className = 'main'>  
+            <div className = 'frame'>
+              <a href = 'https://www.instagram.com/jiselacorona/' > 
+                <img id = 'profile' src = './Profile.jpeg' alt = 'me' />
+              </a>
+            </div>
+            <p id ='name'> Jisela Corona <span> 25 </span></p>
+              <p id = 'state'> California </p>
+     </div>
+  )
+}
